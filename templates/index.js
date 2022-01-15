7bc0a29ca10b3b43cc083f930ac98de8df9e780a
Gurveer Sohal <gurveersohal433@gmail.com>
2022-01-15 16:29:55 -0700

    mapbox

--- templates/index.js
+++ templates/index.js
@@ -3,15 +3,23 @@ import TileLayer from 'ol/layer/Tile';
 import {Map, View} from 'ol';
 import {fromLonLat} from 'ol/proj';
 
-new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: fromLonLat([0, 0]),
-    zoom: 2,
-  }),
+// new Map({
+//   target: "map",
+//   layers: [
+//     new TileLayer({
+//       source: new OSM(),
+//     }),
+//   ],
+//   view: new View({
+//     center: fromLonLat([0, 0]),
+//     zoom: 2,
+//   }),
+// });
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJpZW5kbHliYWtlciIsImEiOiJja3lnZnE5dnkxMGt3MnJtazMzdzV2NXZsIn0.M1eGhDgpm4VzVswVRExBxQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
 });
