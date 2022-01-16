7b2ebd70d05b44af6631b8e07ed63ed891b28f49
shehraj123 <shehrajsingh@gmail.com>
2022-01-16 02:48:28 -0700

    Changed front-end

--- main/views.py
+++ main/views.py
@@ -6,16 +6,25 @@ def index(request):
     return render(request, "index.html")
 
 def map(request):
-    if map.method == "POST":
+    if request.method == "POST":
         pickup = request.POST.get('pickup')
         destination = request.POST.get('dest')
-        passengers = request.POST.get('passengers')
+        passengers = request.POST.get('passcount')
+        print(passengers)
+        l = ['', None]
+        if pickup in l or passengers in l or destination in l:
+            return render(request, "index.html")
         pickup_lat, pickup_long = pickup.split(', ')
+        pickup_lat = float(pickup_lat)
+        pickup_long = float(pickup_long)
         dest_lat, dest_long = destination.split(', ')
+        dest_lat = float(dest_lat)
+        dest_long = float(dest_long)
+        passengers = int(passengers)
         pr = interface.Predictor("model.pkl")
         pred = pr.predictCost(pickup_long, pickup_lat, dest_long, dest_lat, passengers)
     context = {
-        'predictedcost':  '$ {}'.format(pred) # {{predictedcost}}
+        'predictedcost':  '$ {%.2f}'.format(pred) # {{predictedcost}}
     }
 
     return render(request, "result.html", context) 
