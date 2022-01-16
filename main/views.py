8b4abd0e6f734ec0a7cff795c555d3c533817d66
shehraj123 <shehrajsingh@gmail.com>
2022-01-16 03:30:10 -0700

    Read file coord

--- main/views.py
+++ main/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 import interface
-
+import os
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def map(request):
     if request.method == "POST":
-        pickup = request.POST.get('pickup')
-        destination = request.POST.get('dest')
+        coor = open('../../coordinates.txt', 'r')
+        pickup = coor.readline()
+        destination = coor.readline()
         passengers = request.POST.get('passcount')
         print(passengers)
         l = ['', None]
         if pickup in l or passengers in l or destination in l:
             return render(request, "index.html")
-        pickup_lat, pickup_long = pickup.split(', ')
+        pickup_lat, pickup_long = pickup.split(',')
         pickup_lat = float(pickup_lat)
         pickup_long = float(pickup_long)
-        dest_lat, dest_long = destination.split(', ')
+        dest_lat, dest_long = destination.split(',')
         dest_lat = float(dest_lat)
         dest_long = float(dest_long)
         passengers = int(passengers)
